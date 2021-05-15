a0f3a3bba810a27654a3bd2b929773c345aaff7d
Drake Vincent <83470503+drakevincent@users.noreply.github.com>
2021-05-14 20:01:51 -0700

    Add files via upload

---
+++ ArrayListPrac/Coin.java
@@ -0,0 +1,22 @@
+public class Coin {
+
+    private double myValue;
+    private String myName;
+
+    public Coin(double value, String name) {
+        myValue = value;
+        myName = name;
+    }
+
+    public double getValue() {
+        return myValue;
+    }
+
+    public String getName() {
+        return myName;
+    }
+
+    public boolean equals(Coin aCoin) {
+        return aCoin.getName().equals(myName) && aCoin.getValue() == myValue;
+    }
+}
